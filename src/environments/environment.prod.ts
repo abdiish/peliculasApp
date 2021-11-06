012b754a1e7c848801a0f3f51e3f46f842dd51cf
Alan Cortes <ing.ti.upmh@gmail.com>
2021-11-05 20:45:50 -0600

    Optimización de código y variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '8a05395ea724c063ec59fe2970f1c730',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '8a05395ea724c063ec59fe2970f1c730',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
