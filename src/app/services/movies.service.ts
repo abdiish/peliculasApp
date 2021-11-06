a172e4aa552e752ddf267121869a4d308e324db4
Alan Cortes <ing.ti.upmh@gmail.com>
2021-11-05 19:30:33 -0600

    Creación de servicio, importar httpClient Module y mostrar en consola resultados al consumir servicio

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeature(){
+
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2020-01-01&primary_release_date.lte=2021-12-31&api_key=8a05395ea724c063ec59fe2970f1c730&language=es&include_image_language=es`);
+
+  }
+
+}
